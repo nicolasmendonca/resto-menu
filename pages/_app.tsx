de2fb938ccd6cd513bf033b3afc2fe229edd1f9f
Niko Mendo <nicolasmendonca@gmail.com>
2021-05-16 15:17:02 -0300

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from 'theme';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
+};
+
+export default MyApp;
